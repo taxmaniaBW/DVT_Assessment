a1bccc5bd476b55ee28bd8bfb1bf2b4930d30632
Tax <gofaonemokgethisi@gmail.com>
2024-01-09 18:30:29 +0200

    =structure dependecies, add ktor

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,12 @@
+object Deps {
+    val KTOR_CORE = "io.ktor:ktor-client-core:${Versions.KTOR_CORE}"
+    val KTOR_CIO = "io.ktor:ktor-client-cio:${Versions.KTOR_CIO}"
+    val ANDROID_X_CORE = "androidx.core:core-ktx:${Versions.ANDROID_X_CORE}"
+    val ANDROID_LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.ANDROID_LIFECYCLE}"
+    val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
+    val COMPOSE_UI = "androidx.compose.ui:ui"
+    val COMPOSE_UI_GRAPHICS = "androidx.compose.ui:ui-graphics"
+    val COMPOSE_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview"
+    val MATERIAL3 = "androidx.compose.material3:material3"
+    val JUNIT = "junit:junit:${Versions.JUNIT}"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+
+        const val KTOR_CORE = "2.3.7"
+        const val KTOR_CIO = "2.3.7"
+        const val ANDROID_X_CORE = "1.12.0"
+        const val ANDROID_LIFECYCLE = "2.6.2"
+        const val ACTIVITY_COMPOSE = "1.8.2"
+        const val JUNIT = "4.13.2"
+
+}
