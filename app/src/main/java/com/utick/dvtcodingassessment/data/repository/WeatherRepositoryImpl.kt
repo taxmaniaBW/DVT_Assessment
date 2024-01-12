d324ab14dd3109faead7f2b1a6e78f0806905e13
Tax <gofaonemokgethisi@gmail.com>
2024-01-13 01:33:53 +0200

    Added APIKEY

--- app/src/main/java/com/utick/dvtcodingassessment/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/utick/dvtcodingassessment/data/repository/WeatherRepositoryImpl.kt
@@ -7,6 +7,7 @@ import com.utick.dvtcodingassessment.network.ApiClient
 import com.utick.dvtcodingassessment.network.BASE_URL
 import com.utick.dvtcodingassessment.network.CURRENT
 import com.utick.dvtcodingassessment.network.FORECAST5
+import com.utick.dvtcodingassessment.util.APIKEY
 import com.utick.dvtcodingassessment.util.Either
 import com.utick.dvtcodingassessment.util.Failure
 import io.ktor.client.call.body
@@ -24,7 +25,7 @@ class WeatherRepositoryImpl(private val client : ApiClient,
                     val currentWeatherResponse: CurrentWeatherResponse =
                         client.api.get("$BASE_URL$CURRENT") {
                             url {
-                                parameters.append("appid", "abc123")
+                                parameters.append("appid", APIKEY)
                                 parameters.append("lat", coord.lat.toString())
                                 parameters.append("lon", coord.lon.toString())
 
@@ -45,7 +46,7 @@ class WeatherRepositoryImpl(private val client : ApiClient,
         return try {
             val forecastWeatherResponse : ForecastWeatherResponse = client.api.get("$BASE_URL$FORECAST5") {
                 url {
-                    parameters.append("appid", "abc123")
+                    parameters.append("appid", APIKEY)
                     parameters.append("lat", coord.lat.toString())
                     parameters.append("lon", coord.lon.toString())
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
 }
+
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
